59ff964660646543a4107802456964ed8031c6e3
khaled5005 <khaledamro6@hotmail.com>
2022-03-03 16:22:01 +0200

    DOMbasics

---
+++ main.js
@@ -0,0 +1,8 @@
+const title = document.getElementById('title')
+const subHeaders = document.getElementsByTagName('h2')
+const uniqueHeadres = document.getElementsByClassName('unique1')
+const header = document.querySelectorAll('#superUnique')
+const body = document.getElementsByTagName('body')[0]
+const dynamicElem = document.createElement('div')
+dynamicElem.id = "dynamic"
+body.innerHTML += '<h2> khaled </h2>' 
